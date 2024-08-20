2e9050e3c381854a826376041e24ba9349db8502
Rakesh Kumar <r.nimriya@gmail.com>
2024-08-20 22:57:05 +0530

    Some Practical for HTMLKICK.com Users
    enjoy guys...

--- script.js
+++ script.js
@@ -1,2 +1,26 @@
 
+  // write a function to find the largest number in an array using reduce method
+
+  function findLargestNumber(arr) {
+
+    let largest = arr[0];
+
+    for (let i = 1; i < arr.length; i++) {
+      if (arr[i] > largest) {
+        largest = arr[i];
+      }
+
+      console.log(findLargestNumber([2,4,6,8,10, 12]));
+    }   
+
     
+    return largest;
+
+   
+  
+  }
+
+  // write a function to find the smallest number in an array using reduce method
+
+
+
